dcf9e01223bbc6cb9b7ff9f047fce37989536fb4
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-02-26 17:06:32 +0100

    BST completed

---
+++ binary_search_tree/dll_queue.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('./doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
+        #FIFO
+
+    def enqueue(self, value):
+        self.storage.add_to_head(value)
+        self.size = self.storage.length
+
+    def dequeue(self):
+        value = None
+        if self.size > 0:
+            value = self.storage.remove_from_tail()
+            
+            self.size = self.storage.length
+        return value
+
+
+    def len(self):
+        return self.size
--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,7 @@
+
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +11,12 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.size = 0
+        self.order = DoublyLinkedList()
+        self.storage = dict()
+
+
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +26,18 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        # if the key exists in the storage
+        if key in self.storage:
+            # extract the node from storage at the key
+            node = self.storage[key] # => memory address of a ref to the actual node in the DLL
+            # move the node to end of order
+            self.order.move_to_end(node)
+            # return the value of the value of the node
+            return node.value[1] # => (n, 4)[1] => 4
+        # otherwise
+        else:
+            # return None
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +50,31 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        # if the key exists in the storage (1)
+        if key in self.storage:
+            # extract the node from the storage
+            node = self.storage[key]
+            # set the nodes value to the key value pair
+            node.value = (key, value)
+
+            # call move to end on the order
+            self.order.move_to_end(node)
+            # return from the set method
+            return
+        
+        # if the size is the same as the limit (2)
+        if self.size == self.limit:
+            # delete the storage at the heads value key
+            del self.storage[self.order.head.value[0]] #  del dict1[name]
+            # call remove from head on the order
+            self.order.remove_from_head()
+            # decrement size
+            self.size -= 1
+        
+        # if both cases are false (does not run if (1) is true
+        # call add to tail on the order passing in the key value pair
+        self.order.add_to_tail((key, value))
+        # set the storage at the index of the key to the orders tail
+        self.storage[key] = self.order.tail
+        # increment the size
+        self.size += 1
